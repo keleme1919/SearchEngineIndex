d1d729e326471d1ef1a44531aaea181db088fb47
B Fuentes <b_fuentes@coloradocollege.edu>
2018-05-08 14:56:12 -0600

    SearchEngineIndex
    just comments for what we need to do

---
+++ SearchEngineIndex.java
@@ -0,0 +1,17 @@
+public class SearchEngineIndex {
+
+  public SearchEngineIndex() {
+    //constructor will initiate that hashmap
+  }
+
+  //word = the word the user is trying to search
+  public String getQuery(String word) {
+    //return ArrayList
+  }
+  //method just taking in the root and outputting the filenames in an arraylist
+  // method that takes in that arraylists and loops through all the elements
+  //and uses them as input for jsoup 
+    // returns the arraylist of single words from that specific file
+
+
+}
